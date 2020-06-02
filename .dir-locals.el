62b6ab6d02f69c027bf30df115fa8f5fdd910552
Wanderson Ferreira <iagwanderson@gmail.com>
2020-06-02 02:12:11 -0300

    first commit

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+((nil . ((cider-docker-translations . (("/app/src" . "/home/wand/study/must-do-coding-questions/src")
+                                     ("/app/test" . "/home/wand/study/must-do-coding-questions/test")))
+         (tgt-projects . (((:root-dir "/home/wand/study/must-do-coding-questions")
+                           (:src-dirs "src")
+                           (:test-dirs "test")
+                           (:test-suffixes "_test")))))))
---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to must-do-coding-questions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
